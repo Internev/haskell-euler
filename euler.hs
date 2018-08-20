5d6e8c3d96f12843b79ecd9683656d08a1ee27a1
Neville Neville <nchallinger@gmail.com>
2018-08-20 16:19:29 +1000

    Add solution to problem 2, including fancy fibs list generator

--- euler.hs
+++ euler.hs
@@ -1,2 +1,9 @@
+-- Problem 1, generalised to any to divisors and any limit.
 prob1 :: Int -> Int -> Int -> Int
 prob1 x y limit = sum [a | a <- [1..limit], a `mod` x == 0 || a `mod` y == 0]
+
+-- Problem 2. Using fancy fib generator, any limit.
+prob2 :: Int -> Int
+prob2 limit = sum [a | a <- takeWhile (<= limit) fibs, even a]
+  where
+    fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
