11e6f192c9a2992146f602f854782aff75e62891
Neville Neville <nchallinger@gmail.com>
2018-08-21 16:49:47 +1000

    Add solution to problem 3.

--- euler.hs
+++ euler.hs
@@ -1,9 +1,22 @@
--- Problem 1, generalised to any to divisors and any limit.
+-- Problem 1. Multiples to limit.
+-- Generalised to any to divisors and any limit.
 prob1 :: Int -> Int -> Int -> Int
 prob1 x y limit = sum [a | a <- [1..limit], a `mod` x == 0 || a `mod` y == 0]
 
--- Problem 2. Using fancy fib generator, any limit.
+-- Problem 2. Even Fibonacci numbers.
+-- Using fancy fib generator, any limit.
 prob2 :: Int -> Int
 prob2 limit = sum [a | a <- takeWhile (<= limit) fibs, even a]
   where
     fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+-- Problem 3. Largest Prime Factor.
+-- Biggest hurdle here was the typing for the sqrt bit :S
+prob3 num = foldr (\p acc -> if num `mod` p == 0 && p > acc then p else acc) 0 ps
+  where
+    ps = primes n
+      where
+        n = floor (sqrt (fromIntegral num))
+        primes limit = 2 : primes'
+          where isPrime (p:ps) n = p*p > n || n `rem` p /= 0 && isPrime ps n
+                primes' = 3 : filter (isPrime primes') [5, 7 .. limit]
